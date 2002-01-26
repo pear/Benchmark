5dd2cfb8561278c458dcd9102eeff4a6f5da39d9
Tomas V.V.Cox <cox@php.net>
2002-01-26 21:47:38 +0000

    - Added getOutput() to print the results in human readable format   and display() to show it.   This functions will automatically detect the environment to select   HTML or text output - Added a new param $auto to constructor. When true is passed the   results will be output automatically at the end of the script   execution. (contribution by Ludovico Magnocavallo) - Go with version 1.1 (if Sebastian don't mind) - Added a example test script
    git-svn-id: http://svn.php.net/repository/pear/packages/Benchmark/trunk@68771 c90b9560-bf6c-de11-be94-00142212c4b1

--- Timer.php
+++ Timer.php
@@ -21,34 +21,150 @@
 
 /**
  * Benchmark::Timer
- * 
+ *
  * Purpose:
- * 
+ *
  *     Timing Script Execution, Generating Profiling Information
- * 
- * Example:
- * 
- *     $timer = new Benchmark_Timer;
- * 
+ *
+ * Example with automatic profiling start, stop, and output:
+ *
+ *     $timer =& new Benchmark_Timer(true);
+ *     $timer->setMarker('Marker 1');
+ *
+ * Example without automatic profiling:
+ *
+ *     $timer =& new Benchmark_Timer();
+ *
  *     $timer->start();
  *     $timer->setMarker('Marker 1');
  *     $timer->stop();
- * 
+ *
  *     $profiling = $timer->getProfiling();
- * 
+ *     echo $profiling->getOutput(); // or $timer->display()
+ *
+ * Contributors:
+ * - Ludovico Magnocavallo <ludo@sumatrasolutions.com>
+ *   auto profiling and get_output() method
+ *
  * @author   Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @version  $Revision$
  * @access   public
  */
 
-class Benchmark_Timer {
+require_once 'PEAR.php';
+
+class Benchmark_Timer extends PEAR
+{
     /**
      * Contains the markers
      *
      * @var    array
-     * @access public
+     * @access private
      */
     var $markers = array();
+
+    /**
+     * Auto-start and stop timer
+     *
+     * @var    boolean
+     * @access private
+     */
+    var $auto   = false;
+
+    /**
+     * Max marker name length for non-html output
+     *
+     * @var    integer
+     * @access private
+     */
+    var $strlen_max = 0;
+
+    /**
+     * Constructor, starts profiling recording
+     *
+     * @access public
+     */
+    function Benchmark_Timer($auto = false) {
+        $this->PEAR();
+        if ($auto) {
+            $this->auto = $auto;
+            $this->start();
+        }
+    }
+
+    /**
+     * Destructor, stops profiling recording
+     *
+     * @access private
+     */
+    function _Benchmark_Timer() {
+        if ($this->auto) {
+            $this->stop();
+            $this->display();
+        }
+    }
+
+    /**
+     * Return formatted profiling information.
+     *
+     * @see    getProfiling()
+     * @access public
+     */
+    function getOutput()
+    {
+        if (function_exists('version_compare') &&
+            version_compare(phpversion(), '4.1', 'ge'))
+        {
+            $http = isset($_SERVER['SERVER_PROTOCOL']);
+        } else {
+            global $HTTP_SERVER_VARS;
+            $http = isset($HTTP_SERVER_VARS['SERVER_PROTOCOL']);
+        }
+        $total = $this->TimeElapsed();
+        $result = $this->getProfiling();
+        if ($http) {
+            $out = "<table border=1>\n";
+            $out .= "<tr><td>&nbsp;</td><td><b>time index</b></td><td><b>ex time</b></td><td><b>%</b></td></tr>\n";
+        } else {
+            $dashes = $out = str_pad("\n", ($this->strlen_max + 52), '-', STR_PAD_LEFT);
+            $out .= str_pad('marker', $this->strlen_max);
+            $out .= str_pad("time index", 22);
+            $out .= str_pad("ex time", 22);
+            $out .= "perct\n";
+            $out .= $dashes;
+        }
+        foreach ($result as $k => $v) {
+            $perc = (($v['diff'] * 100) / $total);
+            if ($http) {
+                $out .= "<tr><td><b>" . $v['name'] . "</b></td><td>" . $v['time'] . "</td><td>" . $v['diff'] . "</td><td>" . number_format($perc, 2, '.', '') . "%</td></tr>\n";
+            } else {
+                $out .= str_pad($v['name'], $this->strlen_max, ' ');
+                $out .= str_pad($v['time'], 22);
+                $out .= str_pad($v['diff'], 22);
+                $out .= str_pad(number_format($perc, 2, '.', '') . "%\n", 8, ' ', STR_PAD_LEFT);
+            }
+            $out .= $dashes;
+        }
+        if ($http) {
+            $out .= "<tr style='background: silver;'><td><b>total</b></td><td>-</td><td>${total}</td><td>100.00%</td></tr>\n";
+            $out .= "</table>\n";
+        } else {
+            $out .= str_pad('total', $this->strlen_max);
+            $out .= str_pad('-', 22);
+            $out .= str_pad($total, 22);
+            $out .= "100.00%\n";
+            $out .= $dashes;
+        }
+        return $out;
+    }
+
+    /**
+    * Prints the information returned by getOutput
+    */
+    function display()
+    {
+        print $this->getOutput();
+    }
 
     /**
      * Set "Start" marker.
@@ -110,32 +226,31 @@ class Benchmark_Timer {
      * @access public
      */
     function getProfiling() {
-        $i = 0;
-        $total = 0;
+        $i = $total = $temp = 0;
         $result = array();
-        
+
         foreach ($this->markers as $marker => $time) {
-            if ($marker == 'Start') {
-                $diff = '-';
+            if (extension_loaded('bcmath')) {
+                $diff  = bcsub($time, $temp, 6);
+                $total = bcadd($total, $diff, 6);
             } else {
-                if (extension_loaded('bcmath')) {
-                    $diff  = bcsub($time,  $temp, 6);
-                    $total = bcadd($total, $diff, 6);
-                } else {
-                    $diff  = $time - $temp;
-                    $total = $total + $diff;
-                }
+                $diff  = $time - $temp;
+                $total = $total + $diff;
             }
-            
+
             $result[$i]['name']  = $marker;
             $result[$i]['time']  = $time;
             $result[$i]['diff']  = $diff;
             $result[$i]['total'] = $total;
-            
+
+            $this->strlen_max = (strlen($marker) > $this->strlen_max ? strlen($marker) + 1 : $this->strlen_max);
+
             $temp = $time;
             $i++;
         }
-
+        $result[0]['diff'] = '-';
+        $this->strlen_max = (strlen('total') > $this->strlen_max ? strlen('total') : $this->strlen_max);
+        $this->strlen_max += 4;
         return $result;
     }
 }
