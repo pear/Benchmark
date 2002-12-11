a55ddee36a33edce862e55de9289dc2c34a5b7db
Sebastian Bergmann <sebastian@php.net>
2002-12-11 17:14:17 +0000

    Bump year.
    git-svn-id: http://svn.php.net/repository/pear/packages/Benchmark/trunk@107670 c90b9560-bf6c-de11-be94-00142212c4b1

--- Timer.php
+++ Timer.php
@@ -1,16 +1,16 @@
 <?php
 //
-// +----------------------------------------------------------------------+
-// | PEAR :: Benchmark                                                    |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002 Sebastian Bergmann <sb@sebastian-bergmann.de>.    |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.00 of the PHP License,      |
-// | that is available at http://www.php.net/license/3_0.txt.             |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
+// +------------------------------------------------------------------------+
+// | PEAR :: Benchmark                                                      |
+// +------------------------------------------------------------------------+
+// | Copyright (c) 2001-2003 Sebastian Bergmann <sb@sebastian-bergmann.de>. |
+// +------------------------------------------------------------------------+
+// | This source file is subject to version 3.00 of the PHP License,        |
+// | that is available at http://www.php.net/license/3_0.txt.               |
+// | If you did not receive a copy of the PHP license and are unable to     |
+// | obtain it through the world-wide-web, please send a note to            |
+// | license@php.net so we can mail you a copy immediately.                 |
+// +------------------------------------------------------------------------+
 //
 // $Id$
 //
