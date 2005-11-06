087ca98aa8b85e10c5353f6e18b496e26df58fb6
bertrand Gugger <toggg@php.net>
2005-11-06 09:07:10 +0000

    Prepare release 1.2.4 (stable): Change licence to the new BSD License
    git-svn-id: http://svn.php.net/repository/pear/packages/Benchmark/trunk@200070 c90b9560-bf6c-de11-be94-00142212c4b1

--- Iterate.php
+++ Iterate.php
@@ -5,10 +5,12 @@
 // +------------------------------------------------------------------------+
 // | Copyright (c) 2001-2005 Sebastian Bergmann <sb@sebastian-bergmann.de>. |
 // +------------------------------------------------------------------------+
-// | This source file is subject to version 3.00 of the PHP License,        |
-// | that is available at http://www.php.net/license/3_0.txt.               |
-// | If you did not receive a copy of the PHP license and are unable to     |
-// | obtain it through the world-wide-web, please send a note to            |
+// | This source file is subject to the New BSD license, That is bundled    |
+// | with this package in the file LICENSE, and is available through        |
+// | the world-wide-web at                                                  |
+// | http://www.opensource.org/licenses/bsd-license.php                     |
+// | If you did not receive a copy of the new BSDlicense and are unable     |
+// | to obtain it through the world-wide-web, please send a note to         |
 // | license@php.net so we can mail you a copy immediately.                 |
 // +------------------------------------------------------------------------+
 //
